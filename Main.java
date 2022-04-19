40adc06ae5c81af125a71d19198a933adf25812b
CeliaPe <celia.pedregosa@itgranada.com>
2022-04-19 08:25:45 +0200

    Fin Main

--- Main.java
+++ Main.java
@@ -4,10 +4,12 @@ public class Main {
   public static void main (String[] args){
     String nombre, apellido;
     String numeroString = "10";
-    int numero1 = 10, numero2 = 20;
+    int num, numero1 = 10, numero2 = 20;
+    int dia;
     boolean buleano = true;
     Scanner sc = new Scanner(System.in);
-
+    int[] listaNum = {5,8,7,4,9,5,8,2,4,1,5,4};
+    int[][] matrizNum = {{0,1,2,3}, {4,5,6}, {7,8,9}};
     /*
     System.out.print("Como te llamas? ");
     nombre = sc.nextLine();
@@ -54,6 +56,7 @@ public class Main {
     System.out.println(numero1+numero2+numeroString);
     */
 
+    /*
     if (numero1 < numero2){
       System.out.println("Numero1 es menor.");
     } else if (numero2 < numero1) {
@@ -64,5 +67,58 @@ public class Main {
 
     numeroString = (numero1 < numero2) ? "Numero1 es menor." : "Numero2 es menor";
     System.out.println(numeroString);
+    */
+
+    /*
+    dia = 1;
+    switch (dia){
+      case 1:
+        System.out.println("Hoy es lunes");
+        break;
+      case 2:
+        System.out.println("Hoy es martes");
+        break;
+      case 3:
+        System.out.println("Hoy es miercoles");
+        break;
+      case 4:
+        System.out.println("Hoy es jueves");
+        break;
+      case 5:
+        System.out.println("Hoy es viernes");
+        break;
+      default:
+        System.out.println("El mundo tal y como conocemos ha desaparecido, ya no hay dias de la semana");
+    }
+    */
+
+    /*
+    for (int i = 0 ; i < listaNum.length ; i++){
+      System.out.print(listaNum[i] + " - ");
+    }
+    System.out.println();
+
+    for (int i : listaNum){
+      System.out.print(i + " - ");
+    }
+    System.out.println();
+
+    int i = 0;
+    while (i < listaNum.length){
+      System.out.print(listaNum[i] + " - ");
+      i++;
+    }
+    System.out.println();
+    */
+
+    // int[][] matrizNum = {{0,1,2,3}, {4,5,6}, {7,8,9}}
+    for (int i = 0 ; i < matrizNum.length ; i++){
+      for (int j = 0 ; j < matrizNum[i].length ; j++){
+        System.out.print(matrizNum[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+
   }
 }
