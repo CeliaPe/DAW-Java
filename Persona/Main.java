502cdcc536922bfda37b773b5689e70c38bc51b5
CeliaPe <celia.pedregosa@itgranada.com>
2022-04-20 13:13:25 +0200

    Fin Persona y Alumno

--- Persona/Main.java
+++ Persona/Main.java
@@ -1,7 +1,11 @@
 public class Main{
   public static void main (String[] args){
-    Persona miPersona = new Persona();
+    Persona miPersona = new Persona("P", "98798787k");
     System.out.println(miPersona);
-    
+    System.out.println("El nombre es " + miPersona.getNombre() + ".");
+    System.out.println("El dni es " + miPersona.getDNI() + ".");
+    miPersona.setNombre("Celia");
+    miPersona.setDNI("12312312o");
+    System.out.println(miPersona);
   }
 }
--- Persona/Persona.java
+++ Persona/Persona.java
@@ -1,11 +1,32 @@
 public class Persona{
-  private String nombre;
-  private String dni;
+  protected String nombre;
+  protected String dni;
 
   //Constructores
   public Persona(){
     this.nombre = "Celia";
     this.dni = "12345678H";
+  }
+  public Persona(String nombre, String dni){
+    this.nombre = nombre;
+    this.dni = dni;
+  }
+  public Persona(Persona persona){
+    this.nombre = persona.nombre;
+    this.dni = persona.dni;
+  }
+
+  public String getNombre(){
+    return this.nombre;
+  }
+  public String getDNI(){
+    return this.dni;
+  }
+  public void setNombre(String nombre){
+    this.nombre = nombre;
+  }
+  public void setDNI(String dni){
+    this.dni = dni;
   }
 
   //ToString
