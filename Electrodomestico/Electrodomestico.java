e636ee182bba513ae1de88d2063080177f32ddd8
CeliaPe <celia.pedregosa@itgranada.com>
2022-04-26 09:17:22 +0200

    Comprobaciones

--- Electrodomestico/Electrodomestico.java
+++ Electrodomestico/Electrodomestico.java
@@ -17,6 +17,11 @@ public class Electrodomestico{
   private String consumoEnergetico;
   private double peso;
 
+  // No es necesario utilizar listas dinámicas ni, por tanto, ArrayList.
+  // Se declaran aquí y no dentro de las funciones porque se van a utilizar más de una vez.
+  String colores[] = {"blanco","negro","rojo","azul","metalizado"};
+  String codigos[] = {"F","E","D","C","B","A","AA","AAA"};
+
   // NUEVO -> final static == const
   private final static double PRECIO = 100;
   private final static String COLOR = "blanco"; // Para escribir COLOR cada vez que queramo poner el color por defecto.
@@ -42,7 +47,8 @@ public class Electrodomestico{
     this.precioBase = precioBase;
     //this.color = color;
     this.setColor(color);
-    this.consumoEnergetico = consumoEnergetico;
+    //this.consumoEnergetico = consumoEnergetico;
+    comprobarCodigoEnergetico(consumoEnergetico);
     this.peso = peso;
   }
   public Electrodomestico(Electrodomestico electrodomestico){
@@ -62,7 +68,8 @@ public class Electrodomestico{
     comprobarColor(color);
   }
   public void setConsumoEnergetico(String consumoEnergetico){
-    this.consumoEnergetico = consumoEnergetico;
+    //this.consumoEnergetico = consumoEnergetico;
+    comprobarCodigoEnergetico(consumoEnergetico);
   }
   public void setPeso(double peso){
     this.peso = peso;
@@ -81,7 +88,6 @@ public class Electrodomestico{
   }
 
   private void comprobarColor(String color){
-    String colores[] = {"blanco","negro","rojo","azul","metalizado"};
     boolean encontrado = false;
 
     this.color = COLOR;              //Por defecto.
@@ -93,8 +99,62 @@ public class Electrodomestico{
       }
     }
   }
+  /*
+  private void comprobarCodigoEnergetico(String codigo){
+    boolean encontrado = false;
+    this.consumoEnergetico = CONSUMO;              //Por defecto.
+    codigo = codigo.toUpperCase();
+    for (int i = 0 ; i < codigos.length && !encontrado ; i++){
+      if (codigos[i].equals(codigo)){
+        encontrado = true;
+        this.consumoEnergetico = codigo;
+      }
+    }
+  }
+  */
+  // Podemos mejorar comprobarCodigoEnergetico para que devuelva la posición en la que ha encontrado la letra:
+  private int comprobarCodigoEnergetico(String codigo){
+    boolean encontrado = false;
+
+    this.consumoEnergetico = CONSUMO;              //Por defecto.
+    codigo = codigo.toUpperCase();
+    int i;
+    for (i = 0 ; i < codigos.length && !encontrado ; i++){
+      if (codigos[i].equals(codigo)){
+        encontrado = true;
+        this.consumoEnergetico = codigo;
+      }
+    }
+    return i-1; //Porque si no el i++ devuelve la siguiente posición.
+  }
+
+  public double precioFinal(){
+    return this.precioBase + extraPeso() + extraEficiencia() + extraColor();
+
+  }
+
+  private double extraPeso(){
+    int rango = (int)(this.peso/20);  // Cada 20kg que aumenta el peso
+    return rango*30;                  // El precio aumenta 30€
+  }
+  private double extraEficiencia(){
+    /*
+    |F| E| D| C| B|  A| AA|AAA|
+    |0|20|40|60|80|100|120|140|
+    */
+    // hemos mejorado comprobarCodigoEnergetico() para que devuelva la posición.
+    int posicion = comprobarCodigoEnergetico(this.consumoEnergetico);
+    return posicion*20;
+  }
+  private double extraColor(){
+    double descuento = 0;
+    if ((this.color).equals(COLOR)) descuento = -20;
+    return descuento;
+  }
+
+  // EXTRA: Pensar en la posible refactorización de los métodos comprobarColor() y comprobarCodigoEnergetico()
 
   public String toString(){
-    return "El precio base es  " + this.precioBase + ", el color es " + this.color + ", el consumo es " + this.consumoEnergetico + ", el peso es "+ this.peso + ".";
+    return "El precio base es  " + this.precioBase + ", el color es " + this.color + ", el consumo es " + this.consumoEnergetico + ", el peso es "+ this.peso + ".\nEl precio final es: " + precioFinal() + ".";
   }
 }
