318502d989c08ee2033023528e0ce830430ce766
CeliaPe <celia.pedregosa@itgranada.com>
2022-05-10 08:53:26 +0200

    Estructura ENUM

--- Persona/Alumno.java
+++ Persona/Alumno.java
@@ -4,23 +4,33 @@ public class Alumno extends Persona{
   protected String gradoProt;           // Acceso desde los hijos y el paquete.
   public    String gradoPubl;           // Acceso global.
 
+  enum Nota{
+    SOBRESALIENTE, NOTABLE, BIEN, SUFICIENTE, INSUFICIENTE;
+  }
+
+  Nota nota;
+
   public Alumno(){
     super();
     this.grado = "DAW";
+    this.nota = Nota.SUFICIENTE;
+
   }
   public Alumno(String nombre, String dni, String grado){
     super(nombre,dni);
     this.grado = grado;
+    this.nota = Nota.SUFICIENTE;
   }
   public Alumno(Alumno alumno){
     super(alumno.nombre,alumno.dni);
     this.grado = alumno.grado;
+    this.nota = alumno.nota;
   }
 
   // Gets y Sets
 
   public String toString(){
-    return super.toString() + " Estoy en el grado de " + this.grado + ".";
+    return super.toString() + " Estoy en el grado de " + this.grado + ". Tengo de nota un " + this.nota + ".";
   }
 
 }
--- Persona/Main.java
+++ Persona/Main.java
@@ -1,6 +1,6 @@
 public class Main{
   public static void main (String[] args){
-    Persona miPersona = new Persona("P", "98798787k");
+    Alumno miPersona = new Alumno("P", "98798787k", "DAW");
     System.out.println(miPersona);
     System.out.println("El nombre es " + miPersona.getNombre() + ".");
     System.out.println("El dni es " + miPersona.getDNI() + ".");
