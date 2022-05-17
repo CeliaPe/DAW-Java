8387eadde98586f24739003d13482f2340407244
CeliaPe <celia.pedregosa@itgranada.com>
2022-05-17 09:39:14 +0200

    enum base + habitaciones disponibles

--- Albergue/Albergue.java
+++ Albergue/Albergue.java
@@ -1,14 +1,16 @@
 public enum Albergue{
-  SIMPLE(1,50),
-  DOBLE(2,80),
-  TRIPLE(3,100),
-  MULTIPLE(6,150);
+  SIMPLE(1,50,4),
+  DOBLE(2,80,4),
+  TRIPLE(3,100,4),
+  MULTIPLE(6,150,4);
 
   private int personas;
   private double precio;
+  private int disponibles;
 
-  Albergue(int personas, double precio){
+  Albergue(int personas, double precio, int disponibles){
     this.personas = personas;
     this.precio = precio;
+    this.disponibles = disponibles;
   }
 }
